61222a1dfe127169bf4fd8bc4c2660b45ca1a40e
ManouarRed <139481088+ManouarRed@users.noreply.github.com>
2025-07-05 16:40:01 +0200

    Update create_admin.js

--- create_admin.js
+++ create_admin.js
@@ -9,7 +9,7 @@ async function createNewAdmin() {
       role: 'admin'
     };
 
-    const response = await axios.post('http://localhost:3001/api/users', userData);
+    const response = await axios.post(`${process.env.API_BASE_URL}/api/users`, userData);
     console.log('New admin user created successfully:', response.data);
   } catch (error) {
     console.error('Error creating new admin user:', error.response ? error.response.data : error.message);
