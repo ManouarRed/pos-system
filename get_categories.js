46f20a4565f58a96420b59233316257b10e4b024
ManouarRed <139481088+ManouarRed@users.noreply.github.com>
2025-07-05 16:39:18 +0200

    Update get_categories.js

--- get_categories.js
+++ get_categories.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function getCategories() {
   try {
-    const response = await axios.get('http://localhost:3001/api/categories');
+    const response = await axios.get(`${process.env.API_BASE_URL}/api/categories`);
     console.log('Categories:', response.data.items);
   } catch (error) {
     console.error('Error fetching categories:', error.response ? error.response.data : error.message);
